7ffddfee213bddcab481b8f4f5bc510585f0d0c9
fernandayoga <fernandayoga34@gmail.com>
2026-09-16 12:47:19 +0700

    change model groq

--- api/chat.js
+++ api/chat.js
@@ -192,7 +192,7 @@ export default async function handler(req, res) {
           content: query,
         }
       ],
-      model: "qwen/qwen3.6-27b",
+      model: "llama-3.1-8b-instant",
       temperature: 0.7,
       max_tokens: 4096,
       top_p: 1,
